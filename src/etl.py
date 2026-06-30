720105a695b13a8acb0aebf8ce95f6204ba979ce
James Patrick <jp1252211@gmail.com>
2026-07-01 00:35:32 +0300

    added extract function

--- src/etl.py
+++ src/etl.py
@@ -6,3 +6,7 @@ import os
 # setting up logging configuration
 logging.basicConfig(filename='logs/pipeline.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logging.info('ETL pipeline started.')
+
+#ETL function to extract data from CSV file
+df = pd.read_csv('data/raw_sales.csv')
+logging.info(f"Extracted {len(df)} records.")
