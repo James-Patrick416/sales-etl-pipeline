30fe69d7c1878940f5f6de7fc18de9e013978ba1
James Patrick <jp1252211@gmail.com>
2026-07-01 00:31:10 +0300

    set up log configuration

--- src/etl.py
+++ src/etl.py
@@ -2,3 +2,7 @@
 import pandas as pd
 import logging
 import os
+
+# setting up logging configuration
+logging.basicConfig(filename='logs/pipeline.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.info('ETL pipeline started.')
